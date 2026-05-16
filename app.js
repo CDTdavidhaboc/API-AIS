350b0ddb518a017e25f688fb2c277f0974874ad4
CDTdavidhaboc <23-14890@g.batstate-u.edu.ph>
2026-05-16 11:10:00 +0800

    fix port

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB connected successfully");
 
-        app.listen(process.env.PORT || 1000, () => {
-            console.log(`Server running on port ${process.env.PORT || 5000}`);
+        app.listen(process.env.PORT || 10000, () => {
+            console.log(`Server running on port ${process.env.PORT || 10000}`);
         });
     })
     .catch(error => {

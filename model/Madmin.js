e2e7a902a400c9fcf699a0d6e9380d1356faf751
CDTdavidhaboc <23-14890@g.batstate-u.edu.ph>
2026-05-28 22:30:57 +0800

    fix endpoint

--- model/Madmin.js
+++ model/Madmin.js
@@ -15,11 +15,7 @@ const AdminSchema = new Schema({
         type: String,
         required: true
     },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
+    address: {
         type: String,
         required: true
     }
